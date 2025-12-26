de59274d67135c5573a302b4f8d1731762f606ac
Maslyak <129599445+MrMaslyak@users.noreply.github.com>
2025-12-26 16:57:15 +0200

    start

---
+++ src/main/java/com/example/cuma/CumaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cuma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CumaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cuma/CumaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cuma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
