79eeec86dfe7d91808491a47df4c5b653640e7f9
Maslyak <129599445+MrMaslyak@users.noreply.github.com>
2026-01-13 19:44:07 +0200

    docker compose for customary

---
+++ Customary/src/main/java/com/example/customary/CustomaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.customary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomaryApplication.class, args);
+    }
+
+}
